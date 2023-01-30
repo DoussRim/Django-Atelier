9657c74ed6deb065a48f454223debcf49f3e6bb2
DoussRim <rim.douss@esprit.tn>
2023-01-30 16:17:03 +0100

    S-2

---
+++ Person/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxLengthValidator,MinLengthValidator
+from django.core.exceptions import ValidationError
+# Create your models here.
+def cin_length(v):
+    if len(v)!=8:
+        raise ValidationError("Your CIN must have 8 chara!")
+    return v
+
+def is_mail_esprit(mm):
+    if str(mm).endswith('@esprit.tn')==False:
+        raise ValidationError( "Your email - {m} - must end with esprit.tn",
+                              params={"m":mm})
+    return mm
+class Person(AbstractUser):
+    cin=models.IntegerField('CIN',primary_key=True,validators=[
+                        MaxLengthValidator(8),MinLengthValidator(8),
+                        cin_length])
+    email=models.EmailField(validators=[is_mail_esprit])
+    '''class Meta:
+        verbose_name_plural="users"'''
